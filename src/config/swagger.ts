4722cae7da5b1e60deb90262264eef367e4f2c92
Marcos Dias <marcos.a.melo.dias@gmail.com>
2025-02-14 11:58:20 -0300

    feat: swagger added

---
+++ src/config/swagger.ts
@@ -0,0 +1,19 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+import * as packageJson from '../../package.json'
+
+export class SwaggerConfig {
+    static config(app: INestApplication) {
+        const config = new DocumentBuilder()
+        .setTitle(packageJson.name)
+        .setDescription(packageJson.description)
+        .setVersion(packageJson.version)
+        .build();
+
+        const path = 'docs';
+        const document = SwaggerModule.createDocument(app, config)
+
+        SwaggerModule.setup(path, app, document)
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerConfig } from './config/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  SwaggerConfig.config(app)
+
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
