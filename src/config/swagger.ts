169bf5553e361846c85d3275cfb13bba7eedf32a
Marcos Dias <marcos.a.melo.dias@gmail.com>
2025-02-14 13:41:24 -0300

    style: yarn format

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,19 +1,19 @@
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { INestApplication } from '@nestjs/common'
 
 import * as packageJson from '../../package.json'
 
 export class SwaggerConfig {
-    static config(app: INestApplication) {
-        const config = new DocumentBuilder()
-        .setTitle(packageJson.name)
-        .setDescription(packageJson.description)
-        .setVersion(packageJson.version)
-        .build();
+  static config(app: INestApplication) {
+    const config = new DocumentBuilder()
+      .setTitle(packageJson.name)
+      .setDescription(packageJson.description)
+      .setVersion(packageJson.version)
+      .build()
 
-        const path = 'docs';
-        const document = SwaggerModule.createDocument(app, config)
+    const path = 'docs'
+    const document = SwaggerModule.createDocument(app, config)
 
-        SwaggerModule.setup(path, app, document)
-    }
+    SwaggerModule.setup(path, app, document)
+  }
 }
