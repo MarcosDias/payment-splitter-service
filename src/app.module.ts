a6d95d0b1b550d52b59b2f6f2b72544b37dea3c7
Marcos Dias <marcos.a.melo.dias@gmail.com>
2025-02-14 16:24:06 -0300

    feat: module Database to manage the connection with DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common'
 import { HealthCheckModule } from './health-check/health-check.module'
+import { DatabaseModule } from './database/database.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [HealthCheckModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    HealthCheckModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
--- src/health-check/health-check.controller.spec.ts
+++ src/health-check/health-check.controller.spec.ts
@@ -2,17 +2,34 @@ import { Test, TestingModule } from '@nestjs/testing'
 import request from 'supertest'
 import { INestApplication } from '@nestjs/common'
 import { AppModule } from '../app.module'
+import {
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from '@testcontainers/postgresql';
 
 describe('HealthCheckController', () => {
   let app: INestApplication
+  let postgresContainer: StartedPostgreSqlContainer;
 
   beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile()
+    postgresContainer = await new PostgreSqlContainer().start();
+  }, 20000);
 
-    app = module.createNestApplication()
-    await app.init()
+  beforeEach(async () => {
+    process.env.DB_HOST = postgresContainer.getHost();
+    process.env.DB_PORT = postgresContainer.getPort().toString();
+    process.env.DB_USERNAME = postgresContainer.getUsername();
+    process.env.DB_PASSWORD = postgresContainer.getPassword();
+    process.env.DB_DATABASE = postgresContainer.getDatabase();
+
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        AppModule
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
   })
 
   it('should return status "ok" on GET /health', async () => {
@@ -24,5 +41,6 @@ describe('HealthCheckController', () => {
 
   afterAll(async () => {
     await app.close()
+    await postgresContainer.stop();
   })
 })
